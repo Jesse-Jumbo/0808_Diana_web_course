9ac13ec0a1602397f214174a43563fc03b7a5503
JesseJumbo <jessie97013@gmail.com>
2022-08-08 17:19:44 +0800

    refactor content

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 
+
 app = flask.Flask(__name__)
 
 @app.route("/")
@@ -7,6 +8,7 @@ app = flask.Flask(__name__)
 
 def hello():
     return "Hello, World!"
+
 
 if __name__ == '__main__':
     app.run()
